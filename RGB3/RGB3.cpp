914daec362c4d2caffe7d1525a9591477313f749
casc1244 <1356602646@qq.com>
2020-03-04 22:36:31 +0800

    RGB3.cpp

--- RGB3/RGB3.cpp
+++ RGB3/RGB3.cpp
@@ -1,20 +1,30 @@
-﻿// RGB3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+using namespace cv;
+using namespace std;
+int main(int argc, const char * argv[]) {
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+	cv::Mat dispMat = cv::imread("D:\\360downloads\\1.jpg");
+	cv::Point pt;
+	pt.x = 200;
+	pt.y = 200;
+	cv::Point pt1;
+	pt1.x = 150;
+	pt1.y = 150;
+	cv::Point pt2;
+	pt2.x = 250;
+	pt2.y = 250;
+	circle(dispMat, pt, 100, CV_RGB(255, 0, 0), 1, 8, 0);
+	line(dispMat, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
+	cv::Rect rect;
+	rect.x = 100;
+	rect.y = 100;
+	rect.width;
+	rect.height;
+	rectangle(dispMat, rect, CV_RGB(255, 0, 0), 1, 8, 0);
+	cv::imshow("dispMat",dispMat);
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
